3fc1ce26bf7f8cbd7021311b28675c52f4532110
Ansh Sharma <anshattrii12@gmail.com>
2025-08-03 14:45:01 +0530

    .

--- hr-dashboard/src/components/clientLayout.tsx
+++ hr-dashboard/src/components/clientLayout.tsx
@@ -4,10 +4,12 @@ import { useEffect, useState } from "react";
 import LeftPanel from "./LeftPanel";
 import { setSideBar } from "@/utils/store/sidebar";
 import { useAppDispatch, useAppSelector } from "@/utils/store";
+import { usePathname } from "next/navigation";
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   const sideBar = useAppSelector((state) => state.sidebar);
   const dispatch = useAppDispatch();
   const [hasToken, setHasToken] = useState(false);
+  const pathname = usePathname()
   useEffect(() => {
     const handleResize = () => {
       dispatch(setSideBar(window.innerWidth >= 1100));
@@ -18,10 +20,11 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
 
     return () => window.removeEventListener("resize", handleResize);
   }, []);
-    
+  
+  const shouldShowSidebar = pathname !== "/" && sideBar;
   return (
     <div className="main-content">
-        {sideBar && (
+        {shouldShowSidebar && (
               <div className="left-panel">
                 <LeftPanel />
               </div>
